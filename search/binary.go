5a7490b16def25d4f5dcfcb3f720ce110cffa04f
João Dias <joao.dias1397@gmail.com>
2024-04-06 23:41:18 +0100

    feat: adds binary iteractive algorithm

---
+++ search/binary.go
@@ -0,0 +1,20 @@
+package search
+
+func Binary(items []int, value int) int {
+	low := 0
+	high := len(items) - 1
+
+	for low < high {
+		mid := int(low + (high-low)/2)
+
+		if items[mid] == value {
+			return mid
+		} else if items[mid] > value {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
---
+++ search/binary_test.go
@@ -0,0 +1,36 @@
+package search
+
+import (
+	"testing"
+)
+
+func TestBinary_ElementPresent(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	value := 3
+
+	actual := Binary(items, value)
+	if actual != 2 {
+		t.Errorf("Binary search(%v, %d) = %d: want 2", items, value, actual)
+	}
+}
+
+func TestBinary_ElementNotPresent(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	value := 25
+
+	actual := Binary(items, value)
+	if actual != -1 {
+		t.Errorf("Linear search(%v, %d) = %d: want -1", items, value, actual)
+	}
+}
+
+func BenchmarkBinary(b *testing.B) {
+	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	value := 3
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Binary(items, value)
+	}
+}
