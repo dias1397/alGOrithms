fdc5fc75f3b5f97b052e3aab41e6a037dd308c46
João Dias <joao.dias1397@gmail.com>
2024-04-07 22:04:35 +0100

    feature: adds implementation for binary recursive algorithm

--- search/binary.go
+++ search/binary.go
@@ -1,11 +1,30 @@
 package search
 
 func Binary(items []int, value int) int {
+	return binary(items, value, 0, len(items)-1)
+}
+
+func binary(items []int, value, low, high int) int {
+	if low > high {
+		return -1
+	}
+
+	mid := int(low + ((high - low) / 2))
+	if items[mid] == value {
+		return mid
+	} else if items[mid] > value {
+		return binary(items, value, low, mid-1)
+	} else {
+		return binary(items, value, mid+1, high)
+	}
+}
+
+func BinaryIterative(items []int, value int) int {
 	low := 0
 	high := len(items) - 1
 
 	for low <= high {
-		mid := int(low + (high-low)/2)
+		mid := int(low + ((high - low) / 2))
 
 		if items[mid] == value {
 			return mid
--- search/binary_test.go
+++ search/binary_test.go
@@ -28,6 +28,30 @@ func TestBinary(t *testing.T) {
 	}
 }
 
+func TestBinaryIteractive(t *testing.T) {
+	testCases := []struct {
+		name  string
+		items []int
+		value int
+		want  int
+	}{
+		{"Element at the beginning", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0},
+		{"Element in the middle", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4},
+		{"Element at the end", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9},
+		{"Element not present", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 25, -1},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := BinaryIterative(tc.items, tc.value)
+
+			if got != tc.want {
+				t.Errorf("Iteractive binary search(%v, %d) = %d: want %d", tc.items, tc.value, got, tc.want)
+			}
+		})
+	}
+}
+
 func BenchmarkBinary(b *testing.B) {
 	testCases := []struct {
 		name  string
@@ -44,6 +68,27 @@ func BenchmarkBinary(b *testing.B) {
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				_ = Binary(tc.items, tc.value)
+			}
+		})
+	}
+}
+
+func BenchmarkBinaryIteractive(b *testing.B) {
+	testCases := []struct {
+		name  string
+		items []int
+		value int
+	}{
+		{"Element at the beginning", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1},
+		{"Element in the middle", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5},
+		{"Element at the end", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10},
+		{"Element not present", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 25},
+	}
+
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = BinaryIterative(tc.items, tc.value)
 			}
 		})
 	}
