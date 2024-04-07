26abc25bdf5dbd0eea213e535afa8fa363a31406
João Dias <joao.dias1397@gmail.com>
2024-04-07 21:27:30 +0100

    test: change linear_test to use subtests

--- search/linear_test.go
+++ search/linear_test.go
@@ -4,33 +4,47 @@ import (
 	"testing"
 )
 
-func TestLinear_ElementPresent(t *testing.T) {
-	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	value := 8
-
-	actual := Linear(items, value)
-	if actual != 7 {
-		t.Errorf("Linear search(%v, %d) = %d: want 2", items, value, actual)
+func TestLinear(t *testing.T) {
+	testCases := []struct {
+		name  string
+		items []int
+		value int
+		want  int
+	}{
+		{"Element at the beginning", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0},
+		{"Element in the middle", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4},
+		{"Element at the end", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9},
+		{"Element not present", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 25, -1},
 	}
-}
 
-func TestLinear_ElementNotPresent(t *testing.T) {
-	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	value := 25
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := Linear(tc.items, tc.value)
 
-	actual := Linear(items, value)
-	if actual != -1 {
-		t.Errorf("Linear search(%v, %d) = %d: want -1", items, value, actual)
+			if got != tc.want {
+				t.Errorf("Linear search(%v, %d) = %d: want %d", tc.items, tc.value, got, tc.want)
+			}
+		})
 	}
 }
 
 func BenchmarkLinear(b *testing.B) {
-	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	value := 8
+	testCases := []struct {
+		name  string
+		items []int
+		value int
+	}{
+		{"Element at the beginning", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1},
+		{"Element in the middle", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5},
+		{"Element at the end", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10},
+		{"Element not present", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 25},
+	}
 
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		Linear(items, value)
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = Linear(tc.items, tc.value)
+			}
+		})
 	}
 }
