bfea310b58bdcdf44513893af97f5b58c73e5167
João Dias <joao.dias1397@gmail.com>
2024-04-22 21:57:12 +0100

    test: increase number of test cases for sort algorithms

--- sort/quick_test.go
+++ sort/quick_test.go
@@ -11,8 +11,13 @@ func TestQuick(t *testing.T) {
 		items []int
 		want  []int
 	}{
-		{"Unordered array", []int{1, 3, 5, 4, 2}, []int{1, 2, 3, 4, 5}},
-		//{"Empty array", []int{}, []int{}},
+		{"Sorted unsigned", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+		{"Reversed unsigned", []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+		{"Sorted signed", []int{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5}, []int{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5}},
+		{"Reversed signed", []int{5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5}, []int{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5}},
+		{"Random signed", []int{3, 5, -1, -2, 1, 0, 2, -5, -3, 4, -4}, []int{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5}},
+		{"Singleton", []int{1}, []int{1}},
+		{"Empty array", []int{}, []int{}},
 	}
 
 	for _, tc := range testCases {
