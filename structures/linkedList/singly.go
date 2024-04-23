d618c7ea598d1222a1144e07b00be1122f283b10
Joao Dias <joao.dias1397@gmail.com>
2024-04-23 17:55:21 +0100

    refactor: singly linked list interface

--- structures/linkedList/singly.go
+++ structures/linkedList/singly.go
@@ -5,29 +5,14 @@ import (
 	"fmt"
 )
 
-type Singly[T any] struct {
+type Singly[T comparable] struct {
 	length int
 	head   *element[T]
 }
 
-type element[T any] struct {
+type element[T comparable] struct {
 	value T
 	next  *element[T]
-}
-
-func (list *Singly[T]) Append(val T) {
-	node := element[T]{value: val}
-
-	if list.head == nil {
-		list.head = &node
-	} else {
-		current := list.head
-		for ; current.next != nil; current = current.next {
-		}
-		current.next = &node
-	}
-
-	list.length++
 }
 
 func (list *Singly[T]) Prepend(val T) {
@@ -46,30 +31,70 @@ func (list *Singly[T]) Prepend(val T) {
 func (list *Singly[T]) InsertAt(val T, index int) error {
 	if index > list.length {
 		return errors.New("Index out of bounds")
-	} else {
-		node := element[T]{value: val}
-
-		current := list.head
-		for i := 0; i < index-1; i++ {
-			current = current.next
-		}
-
-		if current == nil {
-			list.head = &node
-		} else {
-			node.next = current.next
-			current.next = &node
-		}
-
-		list.length++
-
+	} else if index == list.length {
+		list.Append(val)
+		return nil
+	} else if index == 0 {
+		list.Prepend(val)
 		return nil
 	}
+
+	curr := list.head
+	for i := 0; i < index-1; i++ {
+		curr = curr.next
+	}
+
+	node := element[T]{value: val}
+
+	node.next = curr.next
+	curr.next = &node
+	list.length++
+
+	return nil
 }
 
-func (list *Singly[T]) Remove() (element[T], error) {
+func (list *Singly[T]) Append(val T) {
+	node := element[T]{value: val}
 
-	return element[T]{}, nil
+	if list.head == nil {
+		list.head = &node
+	} else {
+		current := list.head
+		for ; current.next != nil; current = current.next {
+		}
+		current.next = &node
+	}
+
+	list.length++
+}
+
+func (list *Singly[T]) Remove(val T) (element[T], error) {
+	curr := list.head
+
+	if curr.value == val {
+		list.head = curr.next
+		list.length--
+		curr.next = nil
+		return *curr, nil
+	}
+
+	found := false
+	for ; curr.next != nil; curr = curr.next {
+		if curr.next.value == val {
+			found = true
+			break
+		}
+	}
+
+	if found {
+		node := curr.next
+		curr.next = node.next
+		list.length--
+		node.next = nil
+		return *node, nil
+	}
+
+	return element[T]{}, errors.New("Element not found in list")
 }
 
 func (list *Singly[T]) RemoveAt(index int) (element[T], error) {
